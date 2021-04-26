c2c83d5f0c289bd18fef5c2470efcfd274c2019a
Hugo Ferreira <6885730+hferreira23@users.noreply.github.com>
2021-04-26 01:55:09 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -7,8 +7,5 @@ if [ -f /usr/local/lib/libipmctl.so ]
 then
   export GO_TAGS=${GO_TAGS},libipmctl
 fi
-echo $TARGETARCH
-export GOARCH=$TARGETARCH
-echo $GOARCH
 export GO_FLAGS="-tags=${GO_TAGS}"
 echo $GO_FLAGS
